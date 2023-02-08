60d0a2a8842a8d2f3bafcd9a5bc51eb398aa1f0a
Heronstares <diman.p.dimitrov@gmail.com>
2023-02-08 11:50:18 +0200

    added emoji to hot price

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,11 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const prices = document.querySelectorAll('.hot')
+  prices.forEach(el=>{
+  
+        el.innerHTML+=' 🔥'
+    })
+    
+
 });
