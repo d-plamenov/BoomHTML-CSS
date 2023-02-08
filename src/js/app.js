65cf3351aca0ec8924de347dff97d9c30f5202d0
Heronstares <diman.p.dimitrov@gmail.com>
2023-02-08 12:34:40 +0200

    added avatar that gets bigger on click

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const imageDiv = document.querySelectorAll(".image")[0];
+  imageDiv.addEventListener("click", () => {
+    imageDiv.style['transform'] = 'scale(2)';
+  })
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
