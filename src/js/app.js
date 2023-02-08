94a4b240065b61dc1759a64a52636cdf1f518cd5
Heronstares <diman.p.dimitrov@gmail.com>
2023-02-08 10:59:17 +0200

    added dynamic attributes to product price

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,9 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+    let productDiv = document.getElementsByClassName("product")[0];
+    let priceDiv = document.getElementsByClassName("price")[0];
+    let price = priceDiv.innerHTML;
 
-
+    productDiv.setAttribute("data-price", price);
 });
